5f0c2dcb6f00f8a8f7aa003e139119982ea59008
Namsters <nagirreks123@gmail.com>
2023-07-10 05:40:05 -0400

    updating git for mobile dev

--- assets/js/quiz-script.js
+++ assets/js/quiz-script.js
@@ -27,7 +27,7 @@ var choices = [{
 	optA: "The Highwind",
 	optB: "The Enterprise",
 	optC: "The Lunar Whale",
-	optD: "The Prima Vista",
+	optD: "The M.S. Prima Vista",
 }, {
 	optA: "A Moogle",
 	optB: "A Chocobo",
@@ -54,14 +54,14 @@ var questions = ['Which of the following characters appear in every Final Fantas
 	'Which of the following characters termed the phrase, “You spoony bard!”?',
 	'Who is the dapper agent of enquiry and self proclaimed inspector from Final Fantasy XIV?',
 	'What is the most appropriate method of dealing with an undead train?',
-	'In Final Fantasy 1 what were the elements of the 4 crystals the warriors of light were trying to save.',
+	'In Final Fantasy I what were the elements of the 4 crystals the warriors of light were trying to save.',
 	'What was the name of the airship that the Tantalus Theatre Troupe used when trying to kidnap Princess Garnet?',
 	'What Final Fantasy critter was Cid Fabool IX, from FFIX, transformed into as punishment for adultery?',
 	'What does Kefka, from FFVI, claim not to be when confronted by Sabin and friends in the Imperial Desert Camp.',
 	'What is Rikku`s, from FFX, greatest fear?',
 	'In Final Fantasy II, Guy was possessed of an important language skill that helped the party complete their journey.  What was that language?'];
 
-var answers = ["Cid", "Tellah", "Hildebrand Manderville", "Both B and C", "Earth,Fire, Water and Air", "The M.s. Prima Vista", "An Oglop", "A Waiter", "Lightning", "Beaver"];
+var answers = ["Cid", "Tellah", "Hildebrand Manderville", "Both B and C", "Earth, Fire, Water, and Air", "The M.S. Prima Vista", "An Oglop", "A Waiter", "Lightning", "Beaver"];
 
 var questionNumber = 0;
 var questionTime = document.getElementById('question');
@@ -72,14 +72,20 @@ var questionD = document.getElementById('optD');
 var result = document.getElementById('outcome');
 var buttonEL = document.getElementsByClassName('btn');
 var choose = ""
+var count = 0;
 
 for (var i = 0; i < buttonEL.length; i++) {
 	buttonEL[i].addEventListener('click', event => {
 		choose = event.target.closest("button")
 		check();
+		count ++;
 		return;
 	});
 };
+
+if (count == questions.length) {
+	window.location.href = "scores.html";
+}
 
 function check() {
 	if (answers[questionNumber] === choose.textContent) {
